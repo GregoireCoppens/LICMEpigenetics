95193f5345a26a5e76a073d70088a27ad1ff5ed8
Grégoire Coppens <gregoire.coppens@gmail.com>
2022-03-30 11:47:41 +0200

    make contrast matrix adapt to absence of categorical or continous variables

--- R/DMP_limma.R
+++ R/DMP_limma.R
@@ -5,7 +5,7 @@
 #' The contrast matrix specifies the comparison of interest (e.g.: Patients vs Controls).
 #' To compute a moderated t-statistic of differential expression, it uses empirical Bayes moderation of the standard error.
 #'
-#' @param GRcset Preprocessed Green-Red methylation set
+#' @param set Preprocessed methylation set, can be genomic methyl set or genomic ratio set.
 #' @param Patient_info Data frame with adjustment variables.
 #' @param cat_vars The variable names in Patient_info that are categorical variables
 #' @param cont_vars The variable names in Patient_info that are continuous variables
@@ -22,44 +22,87 @@
 #'
 #' @export
 #'
-#' @examples
 #'
-DMP_limma <- function(GRcset, Patient_info, cat_vars, cont_vars=c(), Group=NULL){
+#' @importFrom limma makeContrasts
+#' @importFrom limma contrasts.fit
+#' @importFrom limma lmFit
+#' @importFrom limma eBayes
+#' @importFrom limma topTable
+#' @importFrom minfi getM
+#' @importFrom tibble rownames_to_column
+#' @importFrom dplyr filter
+#'
+DMP_limma <- function(set, Patient_info, cat_vars = NULL, cont_vars=NULL, Group=NULL){
   # Define inputs
+  if(class(set) =="GenomicRatioSet") GRcset <- set
+  if(class(set) =="GenomicMethylSet") GRcset <- minfi::ratioConvert(set)
+
+  if(is.null(cat_vars) & !is.null(Group)) cat_vars <- c(Group)
   if(is.null(Group)) Group <- cat_vars[1]
   if(Group %in% cat_vars) cat_vars <- cat_vars[!cat_vars %in% Group]
 
   # Prepare Design
-  design <- model.matrix(
-    as.formula(
-      paste("~0",
-            paste("factor(",Group,")", collapse = "+"),
-            paste(
-              "factor(", cat_vars, ", levels=sort(unique(",cat_vars ,"), decreasing = T))",
-              sep = "",
-              collapse = " + "),
-            paste(cont_vars, collapse = "+"), sep = "+" )),
-    Patient_info)
-  colnames(design) <- colnames(design) %>% gsub(pattern = "factor\\(", replacement = "", x = .) %>%  gsub("\\,.+\\)", "_", .) %>% gsub("\\)", "_", .)
+  if(!is.null(cont_vars)){
+    if(length(cat_vars)>0){
+      design <- model.matrix(
+        as.formula(
+          paste("~0",
+                paste("factor(",Group,")", collapse = "+"),
+                paste(
+                  "factor(", cat_vars, ", levels=sort(unique(",cat_vars ,"), decreasing = T))",
+                  sep = "",
+                  collapse = " + "),
+                paste(cont_vars, collapse = "+"), sep = "+" )),
+        Patient_info)
+    } else{
+      design <- model.matrix(
+        as.formula(
+          paste("~0",
+                paste("factor(",Group,")", collapse = "+"),
+                paste(cont_vars, collapse = "+"), sep = "+" )),
+        Patient_info)
+    }
+  } else {
+    if(length(cat_vars)>0){
+      design <- model.matrix(
+        as.formula(
+          paste("~0",
+                paste("factor(",Group,")", collapse = "+"),
+                paste(
+                  "factor(", cat_vars, ", levels=sort(unique(",cat_vars ,"), decreasing = T))",
+                  sep = "",
+                  collapse = " + "), sep = "+" )),
+        Patient_info)
+    } else {
+      design <- model.matrix(
+        as.formula(
+          paste("~0",
+                paste("factor(",Group,")", collapse = "+"),
+                sep = "+" )),
+        Patient_info)
+    }
+  }
+
+  colnames(design) <-   gsub("\\)", "_", gsub("\\,.+\\)", "_", gsub(pattern = "factor\\(", replacement = "", x = colnames(design))))
 
   ContrastFormula <- paste(colnames(design)[c(2,1)], collapse = "-") %>% as.vector()
-  contMatrix <- makeContrasts(contrasts = ContrastFormula, levels=design)
+  contMatrix <- limma::makeContrasts(contrasts = ContrastFormula, levels=design)
 
   # Calculate
-  fit <- lmFit(getM(GRcset), design)
-  fit2 <- contrasts.fit(fit, contMatrix)
-  fit_EB <- eBayes(fit2)
+  fit <- limma::lmFit(minfi::getM(GRcset), design)
+  fit2 <- limma::contrasts.fit(fit, contMatrix)
+  fit_EB <- limma::eBayes(fit2)
 
-  DMPresult <- topTable(fit_EB,
+  DMPresult <- limma::topTable(fit_EB,
                       n=Inf,
                       coef=1)
 
-  Sign_CpG <- DMPresult %>%
-    rownames_to_column("CpG_name") %>%
-    filter(adj.P.Val<=0.05) %>%
-    .$CpG_name
+  Sign_CpG_df <- DMPresult %>%
+    tibble::rownames_to_column("CpG_name") %>%
+    dplyr::filter(.data$adj.P.Val<=0.05)
+  Sign_CpG <- Sign_CpG_df$CpG_name
 
-  DMPresult_sig <-DMPresult %>% filter(adj.P.Val<=0.05)
+  DMPresult_sig <-DMPresult %>% dplyr::filter(.data$adj.P.Val<=0.05)
 
   return(list(
     DMPresult=DMPresult,
