912dc3defdaf5a90822280eaa45d430cc6c5f959
Grégoire Coppens <gregoire.coppens@gmail.com>
2022-03-30 11:50:32 +0200

    GRcset can also be a methylset and will automatically be converted to a genomic ratioset

--- R/DMR_dmrcate.R
+++ R/DMR_dmrcate.R
@@ -4,12 +4,12 @@
 #' The t-values that remain significant after smoothing and multiple testing, are grouped when they are within Lambda base pairs from each other.
 #' Such a group is called a Differently Methylated Region (DMR).
 #'
-#' @param GRcset Preprocessed Green-Red methylation set
+#' @param set Preprocessed methylation set, can be genomic methyl set or genomic ratio set.
 #' @param DMP Output from `DMP_limma()` function.
 #' @param lambda Gaussian kernel bandwidth for smoothed-function estimation. Also informs DMR bookend definition; gaps >= lambda between significant CpG sites will be in separate DMRs. Support is truncated at 5*lambda. Default is 1000 nucleotides.
 #' @param fdr false discovery rate, default = 0.05.
 #'
-#' @return A a S4 object with statistics for every DMR
+#' @return An S4 object with statistics for every DMR
 #' \itemize{
 #'    \item DMR - `dmrcate()`output, a S4 object with statistics for every DMR
 #'    \item DMRRanges - `extractRanges()` output, a S4 object with Ranges from found DMRs
@@ -18,13 +18,16 @@
 #' @export
 #'
 #' @examples
-DMR_dmrcate <- function(GRcset, DMP, lambda=1000, fdr=0.05){
-  annotation <- cpg.annotate(object = GRcset, datatype = "array", what = "M",
+DMR_dmrcate <- function(set, DMP, lambda=1000, fdr=0.05){
+  if(class(set) =="GenomicRatioSet") GRcset <- set
+  if(class(set) =="GenomicMethylSet") GRcset <- minfi::ratioConvert(set)
+
+  annotation <- DMRcate::cpg.annotate(object = GRcset, datatype = "array", what = "M",
                              analysis.type = "differential", design = DMP$design,
                              contrasts = TRUE, cont.matrix = DMP$contMatrix,
                              coef = colnames(DMP$contMatrix)[1], arraytype = "EPIC", fdr = fdr)
-  DMR <- dmrcate(annotation, lambda=lambda, C=2)
-  DMRRanges <- extractRanges(DMR)
+  DMR <- DMRcate::dmrcate(annotation, lambda=lambda, C=2)
+  DMRRanges <- DMRcate::extractRanges(DMR)
 
   return(list(DMR=DMR, DMRRanges=DMRRanges, annotation=annotation))
 }
